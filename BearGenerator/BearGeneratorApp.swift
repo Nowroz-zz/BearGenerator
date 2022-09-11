71bdda9f93300631ead0025e4b78832b4f0c49a0
Nowroz <isl.nowroz@gmail.com>
2022-09-12 01:40:04 +0600

    Initial Commit

---
+++ BearGenerator/BearGeneratorApp.swift
@@ -0,0 +1,17 @@
+//
+//  BearGeneratorApp.swift
+//  BearGenerator
+//
+//  Created by Nowroz Islam on 12/9/22.
+//
+
+import SwiftUI
+
+@main
+struct BearGeneratorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
