058932dbe637511f37ff6b4045439cb81da03c54
Nowroz <isl.nowroz@gmail.com>
2022-09-12 03:08:48 +0600

    Create Basic Layout
    The app has now a basic non-interactive basic layout.
    
     On branch main
     Changes to be committed:
    	modified:   BearGenerator/ContentView.swift

--- BearGenerator/ContentView.swift
+++ BearGenerator/ContentView.swift
@@ -9,8 +9,103 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            VStack {
+                HStack {
+                    Spacer()
+                    
+                    Button {
+                        
+                    } label: {
+                        Label("Edit", systemImage: "pencil")
+                    }
+                }
+                
+                GeometryReader { geoProxy in
+                    ZStack {
+                        AsyncImage(url: URL(string: "https://placebear.com/200/400")) { phase in
+                            if let image = phase.image {
+                                image
+                                    .resizable()
+                                    .scaledToFit()
+                            } else if phase.error != nil {
+                                Text("There was an error loading the image.")
+                            } else {
+                                ProgressView()
+                            }
+                        }
+                        
+                        VStack {
+                            HStack {
+                                Spacer()
+                                
+                                Button {
+                                    
+                                } label: {
+                                    Image(systemName: "arrow.up.and.down.and.arrow.left.and.right")
+                                }
+                                .tint(.white)
+                            }
+                            
+                            Spacer()
+                        }
+                        .padding()
+                    }
+                    .frame(width: geoProxy.size.width, height: geoProxy.size.height)
+                    .overlay(
+                        RoundedRectangle(cornerRadius: 10)
+                            .stroke(.brown, lineWidth: 3)
+                    )
+                }
+                .padding(.vertical)
+                
+                HStack {
+                    Slider(value: .constant(0.5), in: 0.3...2.0)
+                    
+                    Button {
+                        
+                    } label: {
+                        Image(systemName: "arrow.counterclockwise")
+                    }
+                }
+                .padding(.vertical)
+                
+                VStack {
+                    HStack {
+                        Text("Width")
+                        
+                        TextField("Amount", value: .constant(300), format: .number)
+                            .padding(.horizontal)
+                            .background(.brown)
+                            .clipShape(Capsule())
+                    }
+                    
+                    HStack {
+                        Text("Height")
+                        
+                        TextField("Amount", value: .constant(400), format: .number)
+                            .padding(.horizontal)
+                            .background(.brown)
+                            .clipShape(Capsule())
+                    }
+                }
+                .padding(.vertical)
+                
+                Button("Generate") { }
+                    .buttonStyle(.borderedProminent)
+                    .tint(.secondary)
+                    .padding(.vertical)
+            }
+            .navigationTitle("BearGenerator")
+            .padding(.horizontal)
+            .toolbar {
+                ToolbarItem(placement: .confirmationAction) {
+                    Button("Save") { }
+                }
+            }
+        }
+        .preferredColorScheme(.dark)
+        .tint(.brown)
     }
 }
 
