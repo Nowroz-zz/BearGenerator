87505687f6bba1cc3cd4d9c58c9e39c2d40cd2d1
Nowroz <isl.nowroz@gmail.com>
2022-09-14 03:36:21 +0600

    Create BearImageView File
     On branch main
     Changes to be committed:
    	modified:   BearGenerator.xcodeproj/project.pbxproj
    	new file:   BearGenerator/BearImageView.swift

---
+++ BearGenerator/BearImageView.swift
@@ -0,0 +1,20 @@
+//
+//  BearImageView.swift
+//  BearGenerator
+//
+//  Created by Nowroz Islam on 14/9/22.
+//
+
+import SwiftUI
+
+struct BearImageView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct BearImageView_Previews: PreviewProvider {
+    static var previews: some View {
+        BearImageView()
+    }
+}
