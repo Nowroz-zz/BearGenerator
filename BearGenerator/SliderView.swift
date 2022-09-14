7ff5fc24f5ef1cb028e4790652c7112629698771
Nowroz <isl.nowroz@gmail.com>
2022-09-15 02:52:57 +0600

    Create SliderView.swift File
     On branch main
     Changes to be committed:
    	modified:   BearGenerator.xcodeproj/project.pbxproj
    	new file:   BearGenerator/SliderView.swift

---
+++ BearGenerator/SliderView.swift
@@ -0,0 +1,20 @@
+//
+//  SliderView.swift
+//  BearGenerator
+//
+//  Created by Nowroz Islam on 15/9/22.
+//
+
+import SwiftUI
+
+struct SliderView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SliderView_Previews: PreviewProvider {
+    static var previews: some View {
+        SliderView()
+    }
+}
