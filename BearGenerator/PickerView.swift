73a315953155d3aa88c39e568a24fe62f0cad2e1
Nowroz <isl.nowroz@gmail.com>
2022-09-15 02:41:18 +0600

    Create PickerView.swift File
     On branch main
     Changes to be committed:
    	modified:   BearGenerator.xcodeproj/project.pbxproj
    	new file:   BearGenerator/PickerView.swift

---
+++ BearGenerator/PickerView.swift
@@ -0,0 +1,20 @@
+//
+//  PickerView.swift
+//  BearGenerator
+//
+//  Created by Nowroz Islam on 15/9/22.
+//
+
+import SwiftUI
+
+struct PickerView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct PickerView_Previews: PreviewProvider {
+    static var previews: some View {
+        PickerView()
+    }
+}
