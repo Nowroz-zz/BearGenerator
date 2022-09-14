3da3485e149f0e273364e6810a45ecf7b11e2b90
Nowroz <isl.nowroz@gmail.com>
2022-09-15 03:28:24 +0600

    Create OffsetResetterView File
     On branch main
     Changes to be committed:

---
+++ BearGenerator/OffsetResetterView.swift
@@ -0,0 +1,20 @@
+//
+//  OffsetResetterView.swift
+//  BearGenerator
+//
+//  Created by Nowroz Islam on 15/9/22.
+//
+
+import SwiftUI
+
+struct OffsetResetterView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct OffsetResetterView_Previews: PreviewProvider {
+    static var previews: some View {
+        OffsetResetterView()
+    }
+}
