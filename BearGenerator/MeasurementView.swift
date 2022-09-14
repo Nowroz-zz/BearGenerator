b8f68cc821f6be3b933c516466e0f767d9c8658d
Nowroz <isl.nowroz@gmail.com>
2022-09-15 03:12:56 +0600

    Create MeasurementView.swift File
     On branch main
     Changes to be committed:
    	modified:   BearGenerator.xcodeproj/project.pbxproj
    	new file:   BearGenerator/MeasurementView.swift

---
+++ BearGenerator/MeasurementView.swift
@@ -0,0 +1,20 @@
+//
+//  MeasurementView.swift
+//  BearGenerator
+//
+//  Created by Nowroz Islam on 15/9/22.
+//
+
+import SwiftUI
+
+struct MeasurementView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct MeasurementView_Previews: PreviewProvider {
+    static var previews: some View {
+        MeasurementView()
+    }
+}
